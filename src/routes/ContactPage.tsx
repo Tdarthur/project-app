9256b9244db04a5285c9e0918289ef69dc549d97
Tyler <tdarthur3@gmail.com>
2022-12-15 18:39:11 -0500

    creates contact page without functionality

--- src/routes/ContactPage.tsx
+++ src/routes/ContactPage.tsx
@@ -1,5 +1,107 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+
 type Props = {};
 
 export default function ContactPage({}: Props) {
-    return <></>;
+    return (
+        <div className="flex h-full w-full flex-col items-center justify-center">
+            <strong className="mb-6 italic text-yellow-600">Have a question or want to work together?</strong>
+            <ContactForm />
+        </div>
+    );
+}
+
+function ContactForm() {
+    const formSubmitHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className="flex w-[24rem] flex-col items-center">
+            <ContactTextInput
+                name="name"
+                placeholder="Name"
+            />
+
+            <ContactTextInput
+                name="email"
+                placeholder="Email"
+            />
+
+            <ContactTextInput
+                name="subject"
+                placeholder="Subject"
+                className="rounded-b-none"
+            />
+            <ContactTextAreaInput
+                name="message"
+                placeholder="Message"
+                className="mt-0 rounded-t-none border-t border-t-gray-700 outline-offset-8"
+            />
+
+            <button
+                className="yeehaw hover:scale-101 mt-10 w-fit rounded-sm bg-yellow-600 py-2 px-10 font-bold text-black transition-all duration-300 hover:bg-black hover:text-yellow-600 focus:bg-black focus:text-yellow-600"
+                type="submit"
+                onClick={formSubmitHandler}
+            >
+                Submit
+            </button>
+        </form>
+    );
+}
+
+type ContactTextInputProps = {
+    name: string;
+    placeholder: string;
+    className?: string;
+};
+
+function ContactTextInput({ name, placeholder, className }: ContactTextInputProps) {
+    return (
+        <input
+            type="text"
+            name={name}
+            className={classNames(
+                "mt-2 w-full rounded-sm bg-gray-800 p-2 text-gray-100 placeholder-gray-600 autofill:bg-black focus-visible:outline-0",
+                className
+            )}
+            maxLength={30}
+            placeholder={placeholder}
+        />
+    );
+}
+
+function ContactTextAreaInput({ name, placeholder, className }: ContactTextInputProps) {
+    const [value, setValue] = useState("");
+
+    const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        event.preventDefault();
+
+        setValue(event.target.value);
+    };
+
+    return (
+        <div className="relative w-full">
+            <textarea
+                name={name}
+                className={classNames(
+                    "mt-2 h-52 max-h-96 w-full rounded-sm bg-gray-800 p-2 text-gray-100 placeholder-gray-600 autofill:bg-black focus-visible:outline-0",
+                    className
+                )}
+                maxLength={500}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
+            <span
+                className={classNames(
+                    "absolute -bottom-4 left-0 w-full text-xs",
+                    value.length < 500 ? "text-gray-500" : "text-yellow-600"
+                )}
+            >
+                {500 - value.length} characters remaining
+            </span>
+        </div>
+    );
 }
